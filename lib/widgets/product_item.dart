48564838c614ee4f3a2cb97956d36d5b84963836
ab85hishek <ab85hishek@gmail.com>
2021-04-03 16:22:54 +0530

    update

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -18,7 +18,7 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
-    print('rebuilded');
+    //print('rebuilded');
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
