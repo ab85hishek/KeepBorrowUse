305030fece64a100760f7fcdde6fd2c893884644
ab85hishek <ab85hishek@gmail.com>
2021-05-17 14:15:06 +0530

    added user signup

---
+++ lib/providers/auth.dart
@@ -0,0 +1,28 @@
+import 'dart:ffi';
+
+import 'dart:convert';
+
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  Future<Void> signup(String email, String password) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC_qvZO11XxqXBqFadPwdMvp4Yu_KNdbTw');
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    print(json.decode(response.body));
+  }
+}
